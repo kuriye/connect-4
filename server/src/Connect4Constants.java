a55437f60da5187dc11644fa8630733d6e82ba1f
Boudewijn <boudewijn348@gmail.com>
2018-05-21 16:04:57 +0200

    tic tac toe nu naar connect 4

---
+++ server/src/Connect4Constants.java
@@ -0,0 +1,8 @@
+public interface Connect4Constants {
+    int PLAYER1 = 1;
+    int PLAYER2 = 2;
+    int PLAYER1_WON = 1;
+    int PLAYER2_WON = 2;
+    int DRAW = 3;
+    int CONTINUE = 4;
+}
---
+++ server/src/Connect_4_Server.java
@@ -0,0 +1,81 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Connect_4_Server extends Application implements Connect4Constants {
+    private int sessionNo = 1; // Number a session
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        TextArea taLog = new TextArea();
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(new ScrollPane(taLog), 450, 200);
+        primaryStage.setTitle("TicTacToeServer"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        new Thread( () -> {
+            try {
+                // Create a server socket
+                ServerSocket serverSocket = new ServerSocket(8000);
+                Platform.runLater(() -> taLog.appendText(new Date() +
+                        ": Server started at socket 8000\n"));
+
+                // Ready to create a session for every two players
+                while (true) {
+                    Platform.runLater(() -> taLog.appendText(new Date() +
+                            ": Wait for players to join session " + sessionNo + '\n'));
+
+                    // Connect to player 1
+                    Socket player1 = serverSocket.accept();
+
+                    Platform.runLater(() -> {
+                        taLog.appendText(new Date() + ": Player 1 joined session "
+                                + sessionNo + '\n');
+                        taLog.appendText("Player 1's IP address" +
+                                player1.getInetAddress().getHostAddress() + '\n');
+                    });
+
+                    // Notify that the player is Player 1
+                    new DataOutputStream(
+                            player1.getOutputStream()).writeInt(PLAYER1);
+
+                    // Connect to player 2
+                    Socket player2 = serverSocket.accept();
+
+                    Platform.runLater(() -> {
+                        taLog.appendText(new Date() +
+                                ": Player 2 joined session " + sessionNo + '\n');
+                        taLog.appendText("Player 2's IP address" +
+                                player2.getInetAddress().getHostAddress() + '\n');
+                    });
+
+                    // Notify that the player is Player 2
+                    new DataOutputStream(
+                            player2.getOutputStream()).writeInt(PLAYER2);
+
+                    // Display this session and increment session number
+                    Platform.runLater(() ->
+                            taLog.appendText(new Date() +
+                                    ": Start a thread for session " + sessionNo++ + '\n'));
+
+                    // Launch a new thread for this session of two players
+                    new Thread(new HandleASession(player1, player2)).start();
+                }
+            }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }).start();
+    }
+}
